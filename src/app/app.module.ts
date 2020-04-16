1f3cfbd4f19dd01f4430d188f27a4c4d1d70d5b3
unknown <alextello1@hotmail.com>
2020-04-16 12:27:12 -0600

    Funcionalidad de cambios persistentes en dran n drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DragComponent } from './components/drag/drag.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaisesComponent } from './components/paises/paises.component';
 @NgModule({
-  declarations: [AppComponent, VirtualComponent],
-  imports: [BrowserModule, AppRoutingModule, ScrollingModule],
+  declarations: [AppComponent, VirtualComponent, DragComponent, PaisesComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
