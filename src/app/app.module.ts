9f1678f4b682cb58c06bd7ca3faca959728844d6
unknown <alextello1@hotmail.com>
2020-04-16 11:30:07 -0600

    Botones para navegar en scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, VirtualComponent],
+  imports: [BrowserModule, AppRoutingModule, ScrollingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
